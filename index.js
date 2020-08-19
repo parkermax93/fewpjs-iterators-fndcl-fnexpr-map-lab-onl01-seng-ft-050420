2f74210a0423c7fa8beb0baf10c53e13bf622653
Parker Williamson <parker.williamson@gmail.com>
2020-08-19 20:18:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+const titleCased = () => {
+  return tutorials.map( phrase => {
+    return phrase.split(' ').map( word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+};
+
+let students = ["harry", "ron", "hermione", "ginevra"];
+let rollCall = students.map(student => student + " the wizard")
+
+const robots = [
+  { name: 'Johnny 5', modes: 5, isActivated: false, },
+  { name: 'C3PO', modes: 3, isActivated: false, },
+  { name: 'Sonny', modes: 2.5, isActivated: false, },
+  { name: 'Baymax', modes: 1.5, isActivated: false, },
+];
+ 
+const activatedRobots = robots.map(function (robot) {
+  return Object.assign({}, robot, {
+    modes: robot.modes * 2,
+    isActivated: true,
+  });
+});
+ 
+console.log(activatedRobots);
