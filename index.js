b5c841eb5b811224503be69cf1cf326fba7d9735
Parker Williamson <parker.williamson@gmail.com>
2020-08-19 20:19:12 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map( phrase => {
+    return phrase.split(' ').map( word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
+};
+
